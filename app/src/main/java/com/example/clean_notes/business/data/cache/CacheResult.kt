366e61356b7dd3cc1eba0ab5c0f92b34c4904abe
seyed mahdi <ssmmhh1382@gmail.com>
2020-12-03 19:49:15 +0330

    09-Data layer cache

---
+++ app/src/main/java/com/example/clean_notes/business/data/cache/CacheResult.kt
@@ -0,0 +1,11 @@
+package com.example.clean_notes.business.data.cache
+
+
+sealed class CacheResult<out T> {
+
+    data class Success<out T>(val value: T): CacheResult<T>()
+
+    data class GenericError(
+        val errorMessage: String? = null
+    ): CacheResult<Nothing>()
+}
---
+++ app/src/main/java/com/example/clean_notes/business/data/cache/abstraction/NoteCacheDataSource.kt
@@ -0,0 +1,26 @@
+package com.example.clean_notes.business.data.cache.abstraction
+
+import com.example.clean_notes.business.domain.model.Note
+
+interface NoteCacheDataSource{
+
+    suspend fun insertNote(note: Note): Long
+
+    suspend fun deleteNote(primaryKey: String): Int
+
+    suspend fun deleteNotes(notes: List<Note>): Int
+
+    suspend fun updateNote(primaryKey: String, newTitle: String, newBody: String?): Int
+
+    suspend fun searchNotes(
+        query: String,
+        filterAndOrder: String,
+        page: Int
+    ): List<Note>
+
+    suspend fun searchNoteById(id: String): Note?
+
+    suspend fun getNumNotes(): Int
+
+    suspend fun insertNotes(notes: List<Note>): LongArray
+}

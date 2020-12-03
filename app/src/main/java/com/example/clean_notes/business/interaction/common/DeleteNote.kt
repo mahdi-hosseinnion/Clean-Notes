5783a7cc97d893fa01fa68ed2066d14dd53c58a4
seyed mahdi <ssmmhh1382@gmail.com>
2020-12-03 20:07:18 +0330

    12-Planning use cases

---
+++ app/src/main/java/com/example/clean_notes/business/interaction/common/DeleteNote.kt
@@ -0,0 +1,4 @@
+package com.example.clean_notes.business.interaction.common
+
+class DeleteNote {
+}
---
+++ app/src/main/java/com/example/clean_notes/business/interaction/notelist/SearchNotes.kt
@@ -0,0 +1,4 @@
+package com.example.clean_notes.business.interaction.notelist
+
+class SearchNotes {
+}

aeb67b3636d10368dcaba4a7c8cd7a27d9557851
seyed mahdi <ssmmhh1382@gmail.com>
2020-12-06 19:01:24 +0330

    14-Cache and network error handling

--- app/src/main/java/com/example/clean_notes/business/interaction/notelist/InsertNewNote.kt
+++ app/src/main/java/com/example/clean_notes/business/interaction/notelist/InsertNewNote.kt
@@ -1,7 +1,10 @@
 package com.example.clean_notes.business.interaction.notelist
 
+import com.example.clean_notes.business.data.cache.CacheResponseHandler
 import com.example.clean_notes.business.data.cache.abstraction.NoteCacheDataSource
 import com.example.clean_notes.business.data.network.abstraction.NoteNetworkDataSource
+import com.example.clean_notes.business.data.util.safeApiCall
+import com.example.clean_notes.business.data.util.safeCacheCall
 import com.example.clean_notes.business.domain.model.Note
 import com.example.clean_notes.business.domain.model.NoteFactory
 import com.example.clean_notes.business.domain.state.*
@@ -24,7 +27,7 @@ constructor(
         id: String? = null,
         title: String,
         stateEvent: StateEvent
-    ): Flow<DataState<NoteListViewState>> = flow {
+    ): Flow<DataState<NoteListViewState>?> = flow {
 
         val newNote = noteFactory.createSingleNote(
             id = id,
@@ -32,39 +35,49 @@ constructor(
             body = ""
         )
 
-        val cacheResult = noteCacheDataSource.insertNote(newNote)
-
-        var cacheResponse: DataState<NoteListViewState>? = null
-
-        if (cacheResult > 0) {
-            val viewState = NoteListViewState(newNote = newNote)
-
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_SUCCESS,
-                    uiComponentType = UIComponentType.Toast,
-                    messageType = MessageType.Success
-                ),
-                data = viewState,
-                stateEvent = stateEvent
-            )
-        } else {
-            cacheResponse = DataState.error(
-                response = Response(
-                    message = INSERT_NOTE_FAILURE,
-                    uiComponentType = UIComponentType.Toast,
-                    messageType = MessageType.Error
-                ),
-                stateEvent = stateEvent
-            )
+        val cacheResult = safeCacheCall {
+            noteCacheDataSource.insertNote(newNote)
         }
+
+        val cacheResponse = object : CacheResponseHandler<NoteListViewState, Long>(
+            cacheResult, stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: Long): DataState<NoteListViewState>? {
+                return if (resultObj > 0) {
+                    val viewState = NoteListViewState(newNote = newNote)
+
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast,
+                            messageType = MessageType.Success
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+                    )
+                } else {
+                    DataState.error(
+                        response = Response(
+                            message = INSERT_NOTE_FAILURE,
+                            uiComponentType = UIComponentType.Toast,
+                            messageType = MessageType.Error
+                        ),
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+
+        }.getResult()
+
         emit(cacheResponse)
-        updateNetwork(cacheResponse.stateMessage?.response?.messageType, newNote)
+        updateNetwork(cacheResponse?.stateMessage?.response?.messageType, newNote)
     }
 
     private suspend fun updateNetwork(messageType: MessageType?, newNote: Note) {
         if (messageType == MessageType.Success) {
-            noteNetworkDataSource.insertOrUpdateNote(newNote)
+            safeApiCall {
+                noteNetworkDataSource.insertOrUpdateNote(newNote)
+            }
         }
         //we don,t care about network failure here
     }

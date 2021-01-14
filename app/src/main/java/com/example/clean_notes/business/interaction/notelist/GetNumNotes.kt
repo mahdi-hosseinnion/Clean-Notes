20a101154610e3d8b09f4bfd5e9b6ec8209b6081
seyed mahdi <ssmmhh1382@gmail.com>
2021-01-14 19:04:01 +0330

    20-getNumOfNotes use case

--- app/src/main/java/com/example/clean_notes/business/interaction/notelist/GetNumNotes.kt
+++ app/src/main/java/com/example/clean_notes/business/interaction/notelist/GetNumNotes.kt
@@ -1,4 +1,46 @@
 package com.example.clean_notes.business.interaction.notelist
 
-class GetNumNotes {
+import com.example.clean_notes.business.data.cache.CacheResponseHandler
+import com.example.clean_notes.business.data.cache.abstraction.NoteCacheDataSource
+import com.example.clean_notes.business.data.util.safeCacheCall
+import com.example.clean_notes.business.domain.state.*
+import com.example.clean_notes.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetNumNotes(
+    private val noteCacheDataSource: NoteCacheDataSource
+) {
+
+    fun getNumOfNotes(
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>?> = flow {
+
+        val cacheResult = safeCacheCall {
+            noteCacheDataSource.getNumNotes()
+        }
+
+        val response = object : CacheResponseHandler<NoteListViewState, Int>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: Int): DataState<NoteListViewState>? {
+                return DataState.data(
+                    response = Response(
+                        message = GET_NUM_NOTES_SUCCESS,
+                        uiComponentType = UIComponentType.None,
+                        messageType = MessageType.Success
+                    ),
+                    data = NoteListViewState(numNotesInCache = resultObj),
+                    stateEvent = stateEvent
+                )
+            }
+        }.getResult()
+        emit(response)
+    }
+
+    companion object {
+        val GET_NUM_NOTES_SUCCESS = "Successfully retrieved the number of notes from the cache."
+        val GET_NUM_NOTES_FAILED = "Failed to get the number of notes from the cache."
+    }
 }
